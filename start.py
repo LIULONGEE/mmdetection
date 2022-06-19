654534c0dc65139139a7e77ad8d043eb8a54d2ea
LIULONGEE <36914379+LIULONGEE@users.noreply.github.com>
2022-06-19 18:35:30 +0800

    Update start.py

--- start.py
+++ start.py
@@ -72,8 +72,7 @@ def _run_training(cfg: edict) -> None:
     model_config = cfg.param.model_config
     models_dir = cfg.ymir.output.models_dir
     
-    command = f'CUDA_VISIBLE_DEVICES=0,1,2,3 ' + \
-              f'bash tools/dist_train.sh {model_config} 4 ' + \
+    command = f'bash tools/dist_train.sh {model_config} 4 ' + \
               f'--work-dir {models_dir}'
     logging.info(f'start training: {command}')
     subprocess.run(command.split(), check=True)
